752fb7627c84fae0e266b258941c7d2022fa2854
TylerHager2 <tylerhager02@outlook.com>
2024-06-28 07:24:57 -0700

    test1

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ def main():
     Code Your Program here
     ########################################
     """
+    
+    if number1 < number2 or number1 < number3:
+        min_value = number1
+    elif number2 < number3:
+        min_value = number2
+    else:
+        min_value = number3
+        
+    print(f'{number1} {number2} {number3}\n{min_value}')
 
     ########################################
     # Do not delete the return statement
